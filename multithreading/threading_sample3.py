d94a8ef610b115e881ae2e1f6fb9a175726b1d41
Unlucky935115 <harisheluri9@gmail.com>
2025-04-07 21:25:28 +0530

    new threading code samples

---
+++ multithreading/threading_sample3.py
@@ -0,0 +1,17 @@
+from loguru import logger
+import time
+import threading
+
+def threading_function(name):
+    logger.info("Thread Starting --> {}".format(name))
+    time.sleep(5)
+    logger.info("Thread Finished --> {}".format(name))
+
+if __name__=='__main__':
+    logger.info("MAIN: Before Creating the Thread.")
+    x= threading.Thread(target=threading_function, args=("sample",))
+    logger.info("MAIN: Thread Created and Before running the Thread.")
+    x.start()
+    logger.info("MAIN: Waiting for the thread to finish")
+    x.join()
+    logger.info("MAIN: All done")
