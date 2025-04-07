bdfdfe9d4b9847a534ec5be6622b7c216dd88ef9
Unlucky935115 <harisheluri9@gmail.com>
2025-04-07 19:52:50 +0530

    Python code to generate logs on console as well as in a file

--- Logging/logging_sample5.py
+++ Logging/logging_sample5.py
@@ -1,1 +1,33 @@
 import logging
+from datetime import datetime
+
+current_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+log_filename = "execution_logs/logging_sample5_{}.log".format(current_time)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+formatter =  logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger= logging.getLogger("MyLogger")
+logger.setLevel(level=logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+
+file_handler= logging.FileHandler(log_filename)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def logging_function():
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
+
+logging_function()
